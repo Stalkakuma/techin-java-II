fea24770b8f11c71d32ddf67dc31ec66bbdc485e
Stalkakuma <stalkakuma@gmail.com>
2024-11-20 16:30:58 +0200

    jacoco and some tests added

--- library/src/test/java/lt/vidunas/library/LibraryApplicationTests.java
+++ library/src/test/java/lt/vidunas/library/LibraryApplicationTests.java
@@ -1,21 +1,62 @@
 package lt.vidunas.library;
 
+import lt.vidunas.library.entities.Book;
 import lt.vidunas.library.repositories.BookRepository;
 import lt.vidunas.library.services.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LibraryApplicationTests {
 
-	@MockBean
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
 	private BookRepository bookRepository;
 
 	@Autowired
 	private BookService bookService;
 
+
+	@Test
+	void testGetBook_whenBookExists() throws Exception {
+		Book book = new Book();
+		book.setId(1L);
+		book.setTitle("Book 1");
+
+		//TODO Learn
+//		when(bookService.findBook(1L)).thenReturn(book);
+
+		mockMvc.perform(get("/api/v1/books/1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(1));
+	}
+
+	@Test
+	void testGetBooksEndpoint() throws Exception {
+		Book book = new Book();
+		book.setId(1L);
+		book.setTitle("Book 1");
+		Book book2 = new Book();
+		book2.setId(2L);
+		book2.setTitle("Book 2");
+		bookRepository.save(book);
+		bookRepository.save(book2);
+
+		mockMvc.perform(get("/api/v1/books"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()").value(2));
+	}
 
 	//TODO learn context
 	@Test
